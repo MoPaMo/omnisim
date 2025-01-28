7e66d6ba2ac5c736332ef07bf6953554c2830bcb
MoPaMo <mopamo@users.noreply.github.com>
2025-01-28 11:36:14 +0100

    Enhance Home component styling with tracking adjustments for improved readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div className="">
-      <h1 className="font-sans font-ultrablack text-7xl">OmniSIM</h1>
-      <p className="text-2xl">The best SIM card for your needs</p>
+      <h1 className="font-sans font-ultrablack text-7xl tracking-tighter">OmniSIM</h1>
+      <p className="text-2xl font-mono tracking-tighter">
+        The best SIM card for your needs
+      </p>
     </div>
   );
 }
