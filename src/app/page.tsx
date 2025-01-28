5f17b77676a9eb08e1b7d6157fd62074322ae174
MoPaMo <mopamo@users.noreply.github.com>
2025-01-28 11:39:00 +0100

    Add Globe icon to Home component heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-
+import { Globe } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="">
-      <h1 className="font-sans font-ultrablack text-7xl tracking-tighter">OmniSIM</h1>
+      <h1 className="font-sans font-ultrablack text-7xl tracking-tighter">
+        <Globe className="inline" width={69} height={69} />
+        mniSIM
+      </h1>
       <p className="text-2xl font-mono tracking-tighter">
         The best SIM card for your needs
       </p>
