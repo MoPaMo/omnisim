924678ada2847a3724460e51b9dfd9676bbad370
MoPaMo <mopamo@users.noreply.github.com>
2025-01-30 17:37:57 +0100

    Add additional countries to the countries data for expanded coverage

--- src/data/countries.tsx
+++ src/data/countries.tsx
@@ -3,7 +3,7 @@ interface Country {
   name: string;
   available: boolean;
 }
-
+//please dont get offended by the selection this si just a demo :)
 const countries: Record<string, Country> = {
   US: { id: "US", name: "United States", available: true },
   GB: { id: "GB", name: "United Kingdom", available: true },
@@ -19,6 +19,18 @@ const countries: Record<string, Country> = {
   MX: { id: "MX", name: "Mexico", available: true },
   AR: { id: "AR", name: "Argentina", available: true },
   CL: { id: "CL", name: "Chile", available: true },
+  CN: { id: "CN", name: "China", available: true },
+  IN: { id: "IN", name: "India", available: true },
+  TH: { id: "TH", name: "Thailand", available: true },
+  ID: { id: "ID", name: "Indonesia", available: true },
+  DR: { id: "DR", name: "Dominican Republic", available: true },
+  EG: { id: "EG", name: "Egypt", available: true },
+  GH: { id: "GH", name: "Ghana", available: true },
+  RW: { id: "RW", name: "Rwanda", available: true },
+  KE: { id: "KE", name: "Kenya", available: true },
+  BT: { id: "BT", name: "Bhutan", available: true },
+  BW: { id: "BW", name: "Botswana", available: true },
+  SE: { id: "SE", name: "Sweden", available: true },
 };
 
 export default countries;
